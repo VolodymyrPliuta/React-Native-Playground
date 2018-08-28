a94a4b6931acb1234ff92f5fc75517f6e40f9e4b
americanman <snowboardracer7@gmail.com>
2018-08-28 10:36:18 -0400

    Init commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Yoo world</Text>
+        <Text>I feel good</Text>
+        <Text>Soo good</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
